2d8ebc0bd6aa7fcd94f3c2b396de98e13af45cb0
multihexagon <98978765+multihexagon@users.noreply.github.com>
2024-02-13 18:05:23 -0500

    Inicio

---
+++ index.js
@@ -0,0 +1,68 @@
+const puppeteer = require("puppeteer");
+
+const authenticate = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-gpu", "--disable-setuid-sandbox"],
+  });
+
+  const page = await browser.newPage();
+  const username = "1001370617";
+  const password = "1001370617";
+
+  await page.goto(
+    "https://app.udem.edu.co//ConsultasServAcadem/cargarPaginaLogin.do"
+  );
+
+  const inputElement = await page.$('input[name="login"]');
+  const keyElement = await page.$("[type=password]");
+  const form = await page.$('form[name="loginForm"]');
+
+  await inputElement.type(username);
+  await keyElement.type(password);
+
+  await form.evaluate((form) => form.submit());
+
+  setTimeout(() => {
+    page.goto(
+      "https://app.udem.edu.co/ConsultasServAcadem/asignaturasMat/cargarPaginaConsultar.do"
+    );
+  }, 500);
+
+  setTimeout(async () => {
+    //const aElements = Document.querySelectorAll("a");
+    //const imageElement = await page.$(`img[alt=" Consultar asignaturas matriculadas"]`);
+    //await page.click(imageElement);
+    const links = await page.$$("a");
+
+    // Specify the index of the link you want to click
+    const indexToClick = 16; // Replace with the desired index
+
+    if (indexToClick >= 0 && indexToClick < links.length) {
+      await links[indexToClick].click();
+      console.log(`Clicked on link at index ${indexToClick}`);
+    } else {
+      console.log(`Invalid index: ${indexToClick}`);
+    }
+  }, 1000);
+
+  setTimeout(async () => {
+    //const aElements = Document.querySelectorAll("a");
+    //const imageElement = await page.$(`img[alt=" Consultar asignaturas matriculadas"]`);
+    //await page.click(imageElement);
+    const links = await page.$$("a");
+
+    // Specify the index of the link you want to click
+    const indexToClick = 16; // Replace with the desired index
+
+    if (indexToClick >= 0 && indexToClick < links.length) {
+      await links[indexToClick].click();
+      console.log(`Clicked on link at index ${indexToClick}`);
+    } else {
+      console.log(`Invalid index: ${indexToClick}`);
+    }
+  }, 2000);
+
+
+};
+authenticate();
